6bee4ea242c34c82b915cc2562d41274f746853b
TheBrunix <bruno.petkovic.business@gmail.com>
2024-03-09 18:33:23 +0100

    Dodane funkcionalnosti trenutacnog vremena, datuma i dana u tjednu
    Malo po malo

--- App/main.py
+++ App/main.py
@@ -5,6 +5,7 @@ from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 import speech_recognition as sr
 import pyttsx3
+import datetime
 
 # Inicijalizacija potrebnih stvari 
 recognizer = sr.Recognizer()
@@ -30,7 +31,33 @@ def get_audio():
       except Exception as e:
          print("Exception: " + str(e))
          return ""
-         
+
+# Funkcija za dobivanje trenutacnog vremena
+def tell_current_time():
+   current_time = datetime.datetime.now()
+   current_hours = current_time.strftime("%H")
+   current_minutes = current_time.strftime("%M")
+   if current_hours[0] == "0":
+      current_hours = current_hours[1]
+   if current_minutes[0] == "0":
+      current_minutes = current_minutes[1]
+   print(f"Bot: It is {current_hours} hours and {current_minutes} minutes.")
+   speak(f"It is {current_hours} hours and {current_minutes} minutes.")
+
+# Funkcija za dobivanja trenutacnog dana u tjednu
+def tell_day_of_week():
+   current_date = datetime.datetime.now()
+   day = current_date.strftime("%A")
+   print(f"Bot: It is {day}")
+   speak(f"It is {day}")
+
+# Funkcija za dobivanja trenutacnog datuma
+def tell_current_date():
+   current_date = datetime.datetime.today()
+   formatted_date = current_date.strftime("%Y-%m-%d")
+   print(f"Bot: Today is {formatted_date}")
+   speak(f"Today is {formatted_date}")
+   
 
 def main():
    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -53,8 +80,8 @@ def main():
    model.eval()
 
    bot_name = "Bot"
-   print("Hello! I am your assistant, what can I do?")
-   speak("Hello! I am your assistant, what can I do?")
+   print("Bot: Hello! I am your assistant, what can I do for you?")
+   speak("Hello! I am your assistant, what can I do for you?")
    
    with sr.Microphone() as source:
       recognizer.adjust_for_ambient_noise(source)
@@ -80,10 +107,17 @@ def main():
          for intent in intents["intents"]:
             if tag == intent["tag"]:
                random_choice = random.choice(intent['responses'])
-               print(f"{bot_name}: {random_choice}")
-               speak(random_choice)
+               if random_choice == "":
+                  print(f"{bot_name}: {random_choice}")
+                  speak(random_choice)
          if tag == "goodbye":
             break
+         if tag == "tell-current-time":
+            tell_current_time()
+         if tag == "day_of_week":
+            tell_day_of_week()
+         if tag == "current_date":
+            tell_current_date()
          # ODE NASTAVIT DODAVAT UVJETE A GORE DEFINIRAT VISE FUNKCIJA KOJE IZVRSAVAJU POSLOVE-----------------------------------------------------------
       else:
          print(f"{bot_name}: I do not understand...")     
